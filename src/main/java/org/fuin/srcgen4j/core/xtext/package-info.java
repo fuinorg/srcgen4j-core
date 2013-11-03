cad2ddedfc0785c5d859f9e5cf49da02357e98ab
michael-schnell <michael@fuin.org>
2013-11-03 08:51:37 +0100

    Added Xtext link

--- src/main/java/org/fuin/srcgen4j/core/xtext/package-info.java
+++ src/main/java/org/fuin/srcgen4j/core/xtext/package-info.java
@@ -1,6 +1,6 @@
 // CHECKSTYLE:OFF
 /**
- * Xtext related source code generation.
+ * <a href="http://www.eclipse.org/Xtext/">Xtext</a> related source code generation.
  */
 @XmlSchema(namespace = "http://www.fuin.org/srcgen4j/core/xtext", elementFormDefault = XmlNsForm.QUALIFIED)
 package org.fuin.srcgen4j.core.xtext;
