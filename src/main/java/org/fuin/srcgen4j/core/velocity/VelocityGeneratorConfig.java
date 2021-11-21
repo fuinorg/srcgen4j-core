1684edf13534b1051a0484c192b0c85ac306ae16
Michael Schnell <michael@fuin.org>
2021-11-21 12:33:39 +0100

    Allow sub classing outside of project

--- src/main/java/org/fuin/srcgen4j/core/velocity/VelocityGeneratorConfig.java
+++ src/main/java/org/fuin/srcgen4j/core/velocity/VelocityGeneratorConfig.java
@@ -37,7 +37,7 @@ import org.fuin.srcgen4j.commons.SrcGen4JContext;
  * Minimal configuration for the velocity based generators.
  */
 @XmlAccessorType(XmlAccessType.FIELD)
- abstract class VelocityGeneratorConfig extends AbstractElement
+public abstract class VelocityGeneratorConfig extends AbstractElement
         implements InitializableElement<VelocityGeneratorConfig, Config<GeneratorConfig>> {
 
     @TrimmedNotEmpty
